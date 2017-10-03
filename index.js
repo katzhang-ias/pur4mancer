fd2dd6ae9adf729a47c646befe8983cc27b2542d
kat <kat@integralads.com>
2017-10-03 15:38:47 -0400

    more tests

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ let browser, page;
     browser = await puppeteer.launch({headless: false}); // as of 0.11.0 of puppeteer timeout option doesn't work so have to catch the rejection exceeding default timeout
     page = await browser.newPage();
     await page.tracing.start({path: 'carsales.json', screenshots: true});
-    await page.goto('https://www.motoring.com.au/?mike=pg');
+    page.goto('https://www.motoring.com.au/?mike=pg');
+    await page.waitFor(15*1000);
     await page.tracing.stop();
     await browser.close();
+    console.log('close browser after 15 seconds.');
 })().catch(async e => {
     await page.tracing.stop();
     await browser.close();
